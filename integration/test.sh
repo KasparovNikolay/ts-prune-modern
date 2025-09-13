8307cff2ba1f9f8bc7390a363e3972b1a2253fba
nikolay_kasparov <Kasparov19ya@yandex.ru>
2025-09-13 16:41:11 +0300

    fix tests scripts

--- integration/test.sh
+++ integration/test.sh
@@ -6,23 +6,21 @@ step () {
   printf "\n$STEP_COUNTER. $1 \n"
 }
 
-step "Creating npm link to the current working tree"
-npm link
+step "Build ts-prune"
+cd "$(dirname "$0")/.."
+pnpm build
 
 step "Change to testproject dir"
 cd "$(dirname "$0")"
 cd testproject
 
-step "Linking ts-prune from step 1"
-npm link ts-prune
-
 step "Run ts-prune"
-ts-prune --skip "skip.me" | tee outfile
+node ../../lib/index.js --skip "skip.me" | tee outfile
 
 step "Diff between outputs"
 DIFF=$(diff outfile ../outfile.base)
 EXIT_CODE=2
-if [ "$DIFF" != "" ] 
+if [ "$DIFF" != "" ]
 then
   echo "The output was not the same as the base"
   echo "---"
@@ -35,12 +33,12 @@ else
 fi
 
 step "Run ts-prune with --unusedInModule option"
-ts-prune --skip "skip.me" --unusedInModule | tee outfile_unusedInModules
+node ../../lib/index.js --skip "skip.me" --unusedInModule | tee outfile_unusedInModules
 
 step "Diff between outputs"
 DIFF=$(diff outfile_unusedInModules ../outfile_unusedInModules.base)
 EXIT_CODE=2
-if [ "$DIFF" != "" ] 
+if [ "$DIFF" != "" ]
 then
   echo "The output was not the same as the base"
   echo "---"
@@ -53,37 +51,36 @@ else
 fi
 
 step "Test exit code with no error flag"
-if ! ts-prune > /dev/null; then
+if ! node ../../lib/index.js > /dev/null; then
   echo "ts-prune with no error flag returned error"
   EXIT_CODE=1
 fi
 
 step "Test exit code with error flag"
-if ts-prune -e > /dev/null; then
+if node ../../lib/index.js -e > /dev/null; then
   echo "ts-prune with error flag did not return error"
   EXIT_CODE=1
 fi
 
 step "Test exit code with invalid config path"
-if ts-prune -p ./tsconfig.nonexistens.json &> /dev/null; then
+if node ../../lib/index.js -p ./tsconfig.nonexistens.json &> /dev/null; then
   echo "ts-prune with invalid config path didn't return error"
   EXIT_CODE=1
 fi
 
 step "Test exit code with relative config path"
-if ! ts-prune -p ./tsconfig.json > /dev/null; then
+if ! node ../../lib/index.js -p ./tsconfig.json > /dev/null; then
   echo "ts-prune with relative config path returned error"
   EXIT_CODE=1
 fi
 
 step "Test exit code with absolute config path"
-if ! ts-prune -p $(pwd)/tsconfig.json > /dev/null; then
+if ! node ../../lib/index.js -p $(pwd)/tsconfig.json > /dev/null; then
   echo "ts-prune with absolute config path returned error"
   EXIT_CODE=1
 fi
 
 step "Cleanup"
-rm ../../package-lock.json # remnants of the npm link
 rm outfile # generated outfile
 rm outfile_unusedInModules # generated outfile
 
