e4cca18d9f19e8cbd14ba3993e4aa153735d7b44
nikolay_kasparov <Kasparov19ya@yandex.ru>
2025-09-13 16:34:56 +0300

    update more libs

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  transform: {
+    "^.+\\.ts$": ["ts-jest", {
+      useESM: false,
+    }],
+  },
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/*.d.ts",
+    "!src/**/*.test.ts",
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "html"]
 };
--- src/present.test.ts
+++ src/present.test.ts
@@ -20,9 +20,7 @@ describe("present", () => {
     ].forEach((result) => state.onResult(result));
 
     it("should produce a presentable output", () => {
-      expect(JSON.stringify(present(state))).toMatchInlineSnapshot(
-        `"[\\"\\\\u001b[32mfoo.ts\\\\u001b[39m:\\\\u001b[33m0\\\\u001b[39m - \\\\u001b[36mfoo\\\\u001b[39m\\",\\"\\\\u001b[32mbar.ts\\\\u001b[39m:\\\\u001b[33m0\\\\u001b[39m - \\\\u001b[36mbar\\\\u001b[39m\\"]"`,
-      );
+      expect(JSON.stringify(present(state))).toMatchSnapshot();
     });
   });
 
@@ -64,9 +62,7 @@ describe("present", () => {
     ].forEach((result) => state.onResult(result));
 
     it("should produce a presentable output", () => {
-      expect(JSON.stringify(present(state))).toMatchInlineSnapshot(
-        `"[\\"\\\\u001b[32mfoo.ts\\\\u001b[39m:\\\\u001b[33m0\\\\u001b[39m - \\\\u001b[36mfoo\\\\u001b[39m\\\\u001b[90m (used in module)\\\\u001b[39m\\",\\"\\\\u001b[32mbar.ts\\\\u001b[39m:\\\\u001b[33m0\\\\u001b[39m - \\\\u001b[36mbar\\\\u001b[39m\\"]"`,
-      );
+      expect(JSON.stringify(present(state))).toMatchSnapshot();
     });
   });
 });
